166e766ea6aa1c9f351f7c39ab55835c32802502
JA MacBPro <153228808+joshayala@users.noreply.github.com>
2024-02-01 14:07:17 -0500

    Adding program functions and revising madLibMaterial

--- main.js
+++ main.js
@@ -1,12 +1,29 @@
 // Mixed Messages Project - A Random Message Generator - 'Auto MadLibs'
 // The Array from which the MadLibs will generate
 const madLibMaterial = {
-    Subjects: ['Porcupine', 'Mailmain', 'Walrus', 'Pilot',
-             'Racoon', 'Toddler', 'Orangutan', 'College Student'],
-    Desires: ['wants to', 'aspires to', 'hopes to', 'loves to', 'forgets to', 'hates to'],
-    Action: ['run', 'jump', 'crawl', 'swim', 'play', 'sing'],
-    Place: ['to a hotel', 'on a boat', 'in a river', 'to a restaurant', 
-            'in a playpin', 'at a farmer\'s market', 'in a car'],
+    Subjects: ['porcupine ', 'mailmain ', 'walrus ', 'pilot ',
+             'racoon ', 'toddler ', 'orangutan ', 'college student '],
+    Desires: ['wants to ', 'aspires to ', 'hopes to ', 'loves to ', 
+                'has to ', 'forgets to ', 'hates to '],
+    Action: ['run ', 'jump ', 'crawl ', 'swim ', 'play ', 'sing '],
+    Place: ['to a hotel ', 'on a boat ', 'in a river ', 'to a restaurant ', 
+            'in a playpin ', 'at a farmer\'s market ', 'in a car '],
     Durration: ['weekly', '3 times a week', 'every other month', 
                 '5 times a year', 'once in a lifetime', 'once in a blue moon']
-};
+}
+
+//This Function will randomly select a string from each array within madLibMaterial
+const randomString = (array) => array[Math.floor(Math.random() * array.length)];
+
+//This function will put put all the strings together into a Madlib
+const autoMadlib = () => {
+    let theString = "";
+
+    for (thing in madLibMaterial) {
+        theString += randomString(madLibMaterial[thing]);
+    }
+    console.log("Did you know, every " + theString + "!");
+}
+
+//This Runs the Program
+autoMadlib();
