b3a6261d74fa93459eb6bc932586dbfe90ef3e32
JA MacBPro <153228808+joshayala@users.noreply.github.com>
2024-02-01 13:15:48 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Auto MadLib Generator
+
+## Description
+The Purpose of this project is to build a message generator program that will randomly assign at least 3 different compontents each time the program is run. The Auto MadLib Generator program creates a silly statement about random humans or animals doing random things at random places for random periods of time. The project was developed for the Mixed Messages Project, which is part of the Full-Stack Engineer Career Path in CodeCademy.
+
+## Project Goals
++ To Build a message generator program using JavaScript
++ Use Git Version Control
++ Use the Command Line
++ Develop localy on a computer
+
+## Technologies
++ JavaScript
++ Node.js
+
---
+++ main.js
@@ -0,0 +1,12 @@
+// Mixed Messages Project - A Random Message Generator - 'Auto MadLibs'
+// The Array from which the MadLibs will generate
+const madLibMaterial = {
+    Subjects: ['Porcupine', 'Mailmain', 'Walrus', 'Pilot',
+             'Racoon', 'Toddler', 'Orangutan', 'College Student'],
+    Desires: ['wants to', 'aspires to', 'hopes to', 'loves to', 'forgets to', 'hates to'],
+    Action: ['run', 'jump', 'crawl', 'swim', 'play', 'sing'],
+    Place: ['to a hotel', 'on a boat', 'in a river', 'to a restaurant', 
+            'in a playpin', 'at a farmer\'s market', 'in a car'],
+    Durration: ['weekly', '3 times a week', 'every other month', 
+                '5 times a year', 'once in a lifetime', 'once in a blue moon']
+};
